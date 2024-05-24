d01b68f833dfccba470247cd20657484c65bec6a
etharialle <christopher.m.adams2@gmail.com>
2024-05-24 12:33:11 -0700

    feat: added button to store API key in localStorage

--- index.js
+++ index.js
@@ -4,4 +4,17 @@ fetchButton.addEventListener("click", () => {
         alert("Please Add API Key");
         return
     }
-})
+    console.log(JSON.parse(localStorage.apiKey));
+});
+
+const mainTag = document.querySelector("main");
+//if (!localStorage.apiKey) {}
+const apiKeyButton = document.createElement("button");
+apiKeyButton.textContent = "Add API Key";
+apiKeyButton.id = "api-key-button";
+apiKeyButton.addEventListener("click", () => {
+    const apiKey = prompt("Enter API Key:");
+    localStorage.apiKey = JSON.stringify(apiKey);
+});
+mainTag.appendChild(apiKeyButton);
+
